b63a25d280b404dc42c010df266c3c1534db7c9e
Bryan West <sbw78@cornell.edu>
2020-05-06 00:14:39 -0400

    Fixing directory structure

--- universal-scripts/03_load-data.R
+++ universal-scripts/03_load-data.R
@@ -3,5 +3,5 @@
 
 ### . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . ..
 ### 1. Install packages and standard custom functions                          ####
-source(here::here("command_files/01_install_packages.R"))
-source(here::here("command_files/02_custom_functions.R"))
+source(here::here("command_files/01_install-packages.R"))
+source(here::here("command_files/02_custom-functions.R"))
---
+++ universal-scripts/make-default-dirs.R
@@ -0,0 +1,43 @@
+if (!"here" %in% rownames(utils::installed.packages())) {
+  message("Package `here` is not installed. Installing...")
+  invisible(install.packages("here"))
+} 
+
+start_project <- function(variables) {
+  require(here)
+  if ('here' %in% installed.packages()) {
+    message("Build directories")
+  } else{
+    invisible(install.packages('here'))
+  }
+  
+  subdirs <-
+    c("analysis_data",
+      "command_files",
+      "command_files/functions",
+      "documents",
+      "original_data")
+  sourcedir <- "~/Rmain/etc/universal_scripts/"
+  files <- c("01_install-packages.R", "02_custom-functions.R", "03_load-data.R")
+  destination <- "command_files"
+  
+  for (i in seq_along(subdirs)) {
+    if (file.exists(here::here(subdirs[i]))) {
+      message(paste(subdirs[i], "already exists."))
+      Sys.sleep(0.1)
+    } else {
+      dir.create(here::here(subdirs[i]), showWarnings = FALSE)
+      Sys.sleep(0.1)
+    }
+  }
+  message("Copy standard files over")
+  file.copy(from = paste0(sourcedir, files), 
+            to = here::here(destination, files))
+  
+  file.edit(here::here(destination, files))
+  
+  message("Finished")
+}
+
+start_project()
+rm(list = ls())
